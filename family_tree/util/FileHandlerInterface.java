a28fbb002df5a45bd594b48662b92c95e7ae2306
Olesya2024 <161038064+Olesya2024@users.noreply.github.com>
2024-08-25 20:37:02 +0500

    Delete family_tree directory

--- family_tree/util/FileHandlerInterface.java
+++
@@ -1,8 +0,0 @@
-package family_tree.util;
-
-import java.io.Serializable;
-
-public interface FileHandlerInterface {
-    void writeToFile(String filename, Serializable object);
-    Object readFromFile(String filename);
-}
